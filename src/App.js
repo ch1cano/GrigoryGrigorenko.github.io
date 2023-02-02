b85255d398bf89dd596117c85a48528b2657955d
ch1cano <speedhack5ss@mail.ru>
2023-02-03 00:22:18 +0300

    secound fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/navbar/Navbar';
-import Header from './components/header/Header';
-import Main from './components/main/Main';
 import Footer from './components/footer/Footer';
 import Contacts from './pages/Contacts';
 
