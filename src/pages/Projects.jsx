eb8ecee68cc5c7947dddc6fe6e2aa71314fab015
ch1cano <104975708+ch1cano@users.noreply.github.com>
2023-10-08 13:56:17 +0300

    add projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,13 +5,27 @@ import productsApp from "../img/projects/productu.png"
 import theBotsApp from "../img/projects/TheBots.png"
 import WeatherApp from "../img/projects/React-Weather-App.png"
 import ToDo from "../img/projects/ToDo-react.png"
-import calculatorApp from "../img/projects/calcIphone.png"
+import reactExtension from "../img/projects/extension.png"
+import sonetify from "../img/projects/sonetify.png"
+import psychocon from "../img/projects/psychocon.png"
 
 const Projects = () => {
     return (
         <div className={s.wrapperProj}>
             <div className={s.containerPet}>
                 <h1>Pet Projects</h1>
+                <div>
+                    <h2>Site psychologists</h2>
+                    <img src={psychocon} alt="" width={"1300px"} height={"1200px"} />
+                </div>
+                <div>
+                    <h2>Site consultations</h2>
+                    <img src={sonetify} alt="" width={"1300px"} height={"1200px"} />
+                </div>
+                <div>
+                    <h2>React extension for facebook/instagram</h2>
+                    <img src={reactExtension} alt="" width={"1300px"} height={"1200px"} />
+                </div>
                 <div>
                     <h2>Sneakers Shop</h2>
                     <img src={sneakersShop} alt="" width={"1300px"} height={"1200px"} />
@@ -31,10 +45,6 @@ const Projects = () => {
                 <div>
                     <h2>My To-do list</h2>
                     <img src={ToDo} alt="" width={"1300px"} height={"1200px"} />
-                </div>
-                <div>
-                    <h2>Сalculator Iphone</h2>
-                    <img src={calculatorApp} alt="" width={"500px"} height={"500px"} />
                 </div>
             </div>
         </div>
